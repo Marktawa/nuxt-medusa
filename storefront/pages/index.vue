5da686cddd7e5bfd0bc0eec24d7913a934e47d0e
Mark Munyaka <markmunyaka@gmail.com>
2024-07-28 14:45:03 +0000

    ## Set Up Nuxt
    ### Create a Nuxt project
    
    Open up a new terminal session inside the `nuxt-medusa` directory. Run the following commands:
    
    ```bash
    npx nuxi@latest init storefront
    ```
    
    Answer the prompts as follows:
    
    - Need to install the following packages: nuxi@3.12.0 Ok to proceed?
    `y`
    - Which package manager would you like to use?
    `npm`
    - Nuxt collects completely anonymous data about usage. This will help us improve Nuxt developer experience over time. Read more on https://github.com/nuxt/telemetry. Are you interested in participating?
    `No`
    - Initialize git repository?
    `No`

---
+++ storefront/pages/index.vue
@@ -0,0 +1,16 @@
+// pages/index.vue
+<template>
+  <div>
+    <h1>Welcome to the Medusa Nuxt Store</h1>
+    <h2>Products</h2>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} - ${{ (product.variants[0].prices[0].amount / 100).toFixed(2)}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { data: { products } } = await useFetch('http://localhost:9000/store/products')
+</script>
