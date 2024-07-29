4a7a94bc005204dcfb6c12af9b8cff7edf9b244c
Mark Munyaka <markmunyaka@gmail.com>
2024-07-29 17:53:43 +0000

    ### Add "Add to Cart" Button
    Update each product in `pages/index.vue` with a button to add a product to the cart.
    
    Confirm whether the products are being added to the cart by performing a curl request as follows:
    
    ```bash
    curl localhost:9000/store/carts/cart_01HRBTB0X79NAGJYY8T6D5BGK6
    ```
    
    Replace with the specific `cart id` as listed in your browser console. If all is working your cart should be populated with some products.

--- storefront/pages/index.vue
+++ storefront/pages/index.vue
@@ -6,6 +6,9 @@
     <ul>
       <li v-for="product in products" :key="product.id">
         {{ product.title }} - ${{ (product.variants[0].prices[0].amount / 100).toFixed(2) }}
+        <button @click="addProductToCart(product.variants[0].id)">
+                Add To Cart
+        </button>
       </li>
     </ul>
   </div>
@@ -18,6 +21,24 @@ const runtimeConfig = useRuntimeConfig()
 const MEDUSA_URL = runtimeConfig.public.MEDUSA_URL
 const { data } = await useFetch(`${MEDUSA_URL}/store/products`)
 const products = data.value.products;
+
+async function addProductToCart(variant_id) {
+    const id = localStorage.getItem("cart_id");
+    await fetch(`${MEDUSA_URL}/store/carts/${id}/line-items`, {
+        method: "POST",
+        dentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            variant_id,
+            quantity: 1,
+        }),
+    })
+        .then((response) => response.json())
+        .then(alert('Added to Cart'))
+        //.then(({ cart }) => setCart(cart));
+  }
 
 onMounted(async () => {
     await fetch(`${MEDUSA_URL}/store/carts`, {
